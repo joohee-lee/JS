3fa8587ca315091b6ad8a45272713f14852dae67
joohee-lee <joohee.github@gmail.com>
2018-04-26 23:05:40 +0900

    es5

---
+++ es5/guide.js
@@ -0,0 +1,25 @@
+// 한 줄라인 주석
+//var plus = 1+2;
+//var minus = 2 ? 1
+//  var divide = 3 / 2;
+// 자바스크립트 코드를 작성하기 앞서 주석을 먼저 생각합니다.
+
+/*
+블럭 주석
+* @mehod getValue
+    파라미터로 받은 값에 100을 곱해 반환합니다.
+@param { Number } value, 계산 결과
+*/
+
+function getValue(value){
+    return value * 100;
+}
+
+/*
+getValue(2);
+200
+getValue(3);
+300
+getValue(4);
+400
+*/

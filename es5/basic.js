e0ccad32adf698b89ec2a64769ad263a7563d3cc
joohee-lee <joohee.github@gmail.com>
2018-04-26 23:40:15 +0900

    basic

---
+++ es5/basic.js
@@ -0,0 +1,207 @@
+sports ='수영, 축구';
+sports = '수영,' +
+                '축구';
+
+/*
+"수영,축구"
+*/
+
+var sports = "swim";
+
+// 공백을 토큰
+
+/*
+
+문장부호
+
+{}
+()
+[]
+.
+;
+< > <= >= == != === != !==
++ - * %
+++ --
+<< >>
+>>>
+&
+ㅣ
+^
+!
+~
+&&
+||
+ ? : =
+ +=
+ -=
+ <<=
+ =>>
+ >>>=
+ &=
+ ㅣ=
+ ^=
+ /
+ /=
+
+*/
+
+// ; 세미콜론
+/*
+; 은 자바스크립트 코드를 완성하는 역할
+
+*/
+
+// 에러 - var soccer = 11 var bsketball = 5
+
+var soccer = 11;
+var baketball = 5;
+
+
+// 유니코드
+// http://www.unicode.org
+
+
+// 변수 선언
+// 변수란 값이 변하는 영역
+var sports;
+var soccer;
+
+var sports, soccer;
+var sports,
+    soccer;
+
+//변수 초기값 할당
+츠
+
+var sports = 'swim',
+    soccer = 11;
+
+/*
+sports
+    "swim"
+soccer
+    11
+*/
+
+var sports = 'swim', sports = 'soccer';
+/*
+sports
+    "soccer"
+*/
+var sports = swim = soccer = '스포츠'
+
+/*
+sports
+    "스포츠"
+swim
+    "스포츠"
+soccer
+    "스포츠"
+*/
+
+// 변수 선언은 각각 설정
+
+//상수 : 값이 변하지 않는 영역을 의미 , 항상 고정된 값을 사용하기 위해서, 대문자로
+var ONE = 1
+
+
+// 키워드 , 예약어
+// 키워드란 ? 자바스크립트 기능을 가진 단어.
+/*
+break
+case
+catch
+continue
+debugger <--- es5 add
+default
+delete
+do
+else
+finally
+for
+function
+if
+in
+instanceof
+new
+return
+swich
+this
+throw
+try
+typeof
+var ------ 자바스크립트가  var 키워드를 만나면 변수 선언 기능 수행
+void
+while
+*/
+
+
+//예약어
+// 예약어? 현재 자바스크립트에서 사용하고 있지 않지만 사용할 수 있으므로 예약해둔 단어.
+/*
+ES3  예약어
+abstract
+boolean
+byte
+char
+class
+const
+debugger
+double
+enum
+export
+extends
+final
+float
+goto
+implements
+import
+int
+interface
+long
+native
+package
+private
+protected
+public
+short
+static
+super
+synchronized
+throws
+transient
+volatile
+
+*/
+
+/*
+
+ES5  예약어
+
+class
+const
+enum
+export
+extends
+import
+super
+
+*/
+
+
+/*
+
+ES5  strict 예약어
+
+implements
+interface
+let
+package
+private
+protected
+public
+static
+yield
+
+*/
+
